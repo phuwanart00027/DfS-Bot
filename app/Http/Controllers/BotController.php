3c0cb3dac667924bda2c6e5b86891bea55e6457d
unknown <phuwanart@outlook.co.th>
2019-12-05 14:24:42 +0700

    XX

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -3,26 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\SignatureValidator;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 class BotController extends Controller
 {
     public function botNotify(Request $request){
-        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('nyYah13xqVYr7kkYCpER2mSRTHlamvnscrfxN/HafqG8EnR3U8jxNB/SIFUNQyRBDo1sl6yf5r9pCZ1gjwWlY9fge0k65Bsc5ZvhIK//sX8nJp5mwNJu1WVBRG1RClWhVMWCzGvC5sGn/rk5W57FuAdB04t89/1O/w1cDnyilFU=');
-        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '7665f26dba096fd0e617181e0ee8269f']);
-
-        // $content = file_get_contents('php://input');
+        $httpClient = new CurlHTTPClient('g25x3qWz39fV2KdWySYp/XvYXqyI2QWScXCfsETNWgT+CBMbqB0LmHZWzz2Yi2AYDo1sl6yf5r9pCZ1gjwWlY9fge0k65Bsc5ZvhIK//sX9E66JQZOXujX8Ve+JHEjaLHaD5Htr7Czs/qVHa5KJbLgdB04t89/1O/w1cDnyilFU=');
+        $bot = new LINEBot($httpClient, ['channelSecret' => '7665f26dba096fd0e617181e0ee8269f']);
 
         $events = json_decode($request, true);
         if(!is_null($events)){
-            // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
             $replyToken = $events->events[0]->replyToken;
         }
 
         $textMessageBuilder = new TextMessageBuilder(json_encode($events));
 
-        $response = $bot->replyMessage($replyToken,$textMessageBuilder);
+        $response = $bot->replyText($replyToken,$textMessageBuilder);
         if ($response->isSucceeded()) {
             return 'Succeeded!';
         }
     }
+
+
 }
