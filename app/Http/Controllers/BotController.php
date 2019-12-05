67992f3e4db3a988c9840125f83ea886163fc9b8
unknown <phuwanart@outlook.co.th>
2019-12-05 10:09:40 +0700

    IV

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -6,7 +6,23 @@ use Illuminate\Http\Request;
 
 class BotController extends Controller
 {
-    public function botNotify(){
-        return '555';
+    public function botNotify(Request $request){
+        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('nyYah13xqVYr7kkYCpER2mSRTHlamvnscrfxN/HafqG8EnR3U8jxNB/SIFUNQyRBDo1sl6yf5r9pCZ1gjwWlY9fge0k65Bsc5ZvhIK//sX8nJp5mwNJu1WVBRG1RClWhVMWCzGvC5sGn/rk5W57FuAdB04t89/1O/w1cDnyilFU=');
+        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '7665f26dba096fd0e617181e0ee8269f']);
+
+        $content = file_get_contents('php://input');
+        $events = json_decode($content, true);
+        if (!is_null($events['events'])) {
+            if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+                $text = $event['source']['userId'];
+                $replyToken = $event['replyToken'];
+                $messages = [
+                    'type' => 'text',
+                    'text' => $text
+                ];
+                $response = $bot->replyText($replyToken, $text);
+            }
+
+        }
     }
 }
