fab1683cf2afccf490e53add7d5ad9a98f26d5ce
unknown <phuwanart@outlook.co.th>
2019-12-05 14:07:58 +0700

    XIX

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -6,16 +6,16 @@ use Illuminate\Http\Request;
 
 class BotController extends Controller
 {
-    public function botNotify(){
+    public function botNotify(Request $request){
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('nyYah13xqVYr7kkYCpER2mSRTHlamvnscrfxN/HafqG8EnR3U8jxNB/SIFUNQyRBDo1sl6yf5r9pCZ1gjwWlY9fge0k65Bsc5ZvhIK//sX8nJp5mwNJu1WVBRG1RClWhVMWCzGvC5sGn/rk5W57FuAdB04t89/1O/w1cDnyilFU=');
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '7665f26dba096fd0e617181e0ee8269f']);
 
-        $content = file_get_contents('php://input');
+        // $content = file_get_contents('php://input');
 
-        $events = json_decode($content, true);
+        $events = json_decode($request, true);
         if(!is_null($events)){
             // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
-            $replyToken = $events['events'][0]['replyToken'];
+            $replyToken = $events->events[0]->replyToken;
         }
 
         $textMessageBuilder = new TextMessageBuilder(json_encode($events));
@@ -23,7 +23,6 @@ class BotController extends Controller
         $response = $bot->replyMessage($replyToken,$textMessageBuilder);
         if ($response->isSucceeded()) {
             return 'Succeeded!';
-            // return;
         }
     }
 }
