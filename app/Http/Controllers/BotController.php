031fcedcbdb1af956d9133c2255ac2e0ba1b077c
unknown <phuwanart@outlook.co.th>
2019-12-05 10:31:04 +0700

    V

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class BotController extends Controller
 {
     public function botNotify(Request $request){
+        return '555';
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('nyYah13xqVYr7kkYCpER2mSRTHlamvnscrfxN/HafqG8EnR3U8jxNB/SIFUNQyRBDo1sl6yf5r9pCZ1gjwWlY9fge0k65Bsc5ZvhIK//sX8nJp5mwNJu1WVBRG1RClWhVMWCzGvC5sGn/rk5W57FuAdB04t89/1O/w1cDnyilFU=');
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '7665f26dba096fd0e617181e0ee8269f']);
 
@@ -21,8 +22,15 @@ class BotController extends Controller
                     'text' => $text
                 ];
                 $response = $bot->replyText($replyToken, $text);
+                return $response;
+            }
+            else{
+                return '555';
             }
 
+        }
+        else{
+            return '999';
         }
     }
 }
